4b1302063e04f26c7f297eea3166874b49f04e28
Estela Parrado <estela.parrado@gmail.com>
2018-05-24 10:55:08 +0200

    basic structure

---
+++ README.md
@@ -0,0 +1,8 @@
+##Install:
+
+    npm install
+    npm install yarn
+
+##To run test:
+   ./node_modules/.bin/jest || npm test
+
---
+++ __tests__/calculator.spec.js
@@ -0,0 +1,14 @@
+import Application from '../src/calculator';
+let app;
+
+describe('application', function () {
+
+    beforeEach(function () {
+        app = Application();
+    });
+
+    it('', function () {
+
+    });
+
+});
