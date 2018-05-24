b2043fc810ca91286cf0d5c137db2b507ac39452
Estela Parrado <estela.parrado@gmail.com>
2018-05-24 11:41:06 +0200

    RED sum two values 1 and 0 toBe 1

--- __tests__/calculator.spec.js
+++ __tests__/calculator.spec.js
@@ -8,7 +8,8 @@ describe('calculator', function () {
     });
 
     it('sums two values ', function () {
-        expect(calculator.sum(0, 0)).toBe(0)
+        expect(calculator.sum(0, 0)).toBe(0);
+        expect(calculator.sum(1, 0)).toBe(1);
     });
 
 });
