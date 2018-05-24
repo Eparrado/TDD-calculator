f6d450a1c887aae9afd2aa6fc4ee45bcb1658366
Estela Parrado <estela.parrado@gmail.com>
2018-05-24 13:04:18 +0200

    RED substrat 1 to 0 toBe 0

--- __tests__/calculator.spec.js
+++ __tests__/calculator.spec.js
@@ -11,6 +11,11 @@ describe('calculator', function () {
         expect(calculator.sum(0, 0)).toBe(0);
         expect(calculator.sum(1, 0)).toBe(1);
         expect(calculator.sum(1, 1)).toBe(2);
+        expect(calculator.sum(10, 5)).toBe(15);
+        expect(calculator.sum(500, 500)).toBe(1000);
     });
 
+    it('substract two values', function () {
+        expect(calculator.substract(1, 0)).toBe(0);
+    })
 });
